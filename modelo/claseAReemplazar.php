ea90a6ef94e893d2101242546a2f63e9f2e0c866
Luciano Pecovich <lpecovich@sancristobalsf.com.ar>
2022-09-23 15:07:49 -0300

    Se agrega el codigo propuesto para modificar

--- modelo/claseAReemplazar.php
+++ modelo/claseAReemplazar.php
@@ -4,7 +4,7 @@ class ClaseAReemplazar {
     public static function BuscarTodas()
     {
         $con  = Database::getInstance();
-        $sql = "select * from [ClaseAReemplazar]";
+        $sql = "select * from [tablaAReemplazar]";
         $queryClaseAReemplazar = $con->db->prepare($sql);
         $queryClaseAReemplazar->execute();
         $queryClaseAReemplazar->setFetchMode(PDO::FETCH_CLASS, '[ClaseAReemplazar]');
@@ -21,7 +21,7 @@ class ClaseAReemplazar {
     public static function Buscar($id)
     {
         $con  = Database::getInstance();
-        $sql = "select * from [ClaseAReemplazar] where Id = :p1";
+        $sql = "select * from [tablaAReemplazar] where Id = :p1";
         $queryClaseAReemplazar = $con->db->prepare($sql);
         $params = array("p1" => $id);   
         $queryClaseAReemplazar->execute($params);
@@ -35,10 +35,23 @@ class ClaseAReemplazar {
     public function Agregar()
     {
         $con  = Database::getInstance();
-        $sql = "insert into [ClaseAReemplazar] ([propiedad1],[propiedad2]) values (:p1,:p2)";
+        $sql = "insert into [tablaAReemplazar] ([propiedad1],[propiedad2]) values (:p1,:p2)";
         $claseAReemplazar = $con->db->prepare($sql);
         $params = array("p1" => $this->propiedad1, "p2" => $this->propiedad2);     
         $claseAReemplazar->execute($params);
+    }
+
+    public function Modificar()
+    {
+       $con = Database::getInstance();
+        $sql = "UPDATE [tablaAReemplazar]
+                    SET
+                    [propiedad1] = :p1,
+                    [propiedad2] = :p2
+                    WHERE id = :p3";
+        $autor = $con->db->prepare($sql);
+        $params = array("p1" => $this->propiedad1,"p2" => $this->propiedad2,"p3" => $this->id);
+        $autor->execute($params);
     }
 
 }
